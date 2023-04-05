ef5c16685f60e53ff3716f33951f4a32dfb89139
beefcarhorn <113559659+beefcarhorn@users.noreply.github.com>
2023-04-05 15:23:30 -0400

    u1

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -5,23 +5,19 @@ function Intro() {
 
   return (
     <div className='intro' id='intro'>
-      <div className="intro__left">
-          <div className="image__container">
-            <img className='me' src="assets/me2.png" alt="Me." />
-          </div>
+      <div className="intro__container">
+        <h2>Hi there, I'm</h2>
+        <h1>Aidan Poor</h1>
+        <h3>I'm a
+        <span className='intro__jobs'>
+          <Typewriter loop cursor cursorStyle="|" 
+                      typeSpeed={70} deleteSpeed={50} delaySpeed={2000}
+                      words={[' Software Engineer', ' Web Developer', ' Designer']} />
+        </span>
+        </h3>
       </div>
-      <div className="intro__right">
-        <div className="intro__right__container">
-          <h2 className='intro__h2'>Hi there, I'm</h2>
-          <h1 className='intro__h1'>Aidan</h1>
-          <h3 className='intro__h3'>I'm a
-            <span className='intro__jobs'>
-              <Typewriter loop cursor cursorStyle="|" 
-                          typeSpeed={70} deleteSpeed={50} delaySpeed={2000}
-                          words={[' Software Engineer', ' Web Developer', ' Designer']} />
-            </span>
-          </h3>
-        </div>
+      <div className="me__container">
+        <img className='me2' src="assets/me.png" alt="Me" />
       </div>
     </div>
   )
